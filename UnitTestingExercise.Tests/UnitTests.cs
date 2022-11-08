e6ad3a22f407f0991512645232c171859aba1766
Katelyn Yaeger <knyaeger93@gmail.com>
2022-11-07 18:16:04 -0600

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,60 +6,93 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(2, 3, 5, 10)] //Add test data <------- DONE
+        [InlineData(7, 6, 3, 16)]
+        [InlineData(0, 1, 4, 5)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
 
             //Arrange
-            // create a Calculator object
-            
+                // create a Calculator object - DONE
+
+            var test1 = new Calculator();
+
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+
+            var actual = test1.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+            //Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(7, 8, -1)]//Add test data <------- DONE
+        [InlineData(10, 3, 7)]
+        [InlineData(22, 22, 0)]
+
+
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
 
+            var test2 = new Calculator();
+
             //Act
 
+            var actual = test2.Subtract(minuend, subtrhend);
+
             //Assert
+
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20, 20, 400)]//Add test data <-------
+        [InlineData(40, 0, 0)]
+        [InlineData(2, -3, -6)]
+
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
 
+            var test3 = new Calculator();
+
             //Act
 
+            var actual = test3.Multiple(num1, num2);
+
             //Assert
+
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(24, 8, 3)]//Add test data <-------
+        [InlineData(10, -5, -2)]
+        [InlineData(100, 20, 5)]
+
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
 
-            //Act
+            var test4 = new Calculator();
 
-            //Assert
+            //Act
+            var actual = test4.Divide(num1, num2);
+
+            ////Assert
+            Assert.Equal(expected, actual);
 
         }
 
--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -3,41 +3,62 @@ namespace UnitTestingExercise
 {
     public class Calculator
     {
-        // Step 1: Add a reference to your UnitTestingExercise project:
+        // Step 1: DONE
+        // Add a reference to your UnitTestingExercise project: 
 
-        // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
-        // and add your reference to the UnitTestingExercise project
+        // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project DONE
+        // and add your reference to the UnitTestingExercise project DONE
 
 
 
-        // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer
-        //For Example:
+        // Step 2: DONE
+        // Create an Add method that accepts 3 integers, adds those integers, and returns an integer
+        // For Example: 
         public int Add(int num1, int num2, int num3)
         {
             return num1 + num2 + num3;
         }
 
 
-        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
+        // Step 3: DONE
+        // Navigate to the UnitTests.cs file and complete the AddTest unit test method
 
 
-        // Step 4: 
-        // Create a Subtract method that accepts 2 integers
-        // Keep track of which number is getting passed as minuend and subtrahend
+        // Step 4: DONE
+        // Create a Subtract method that accepts 2 integers 
+        // Keep track of which number is getting passed as minuend and subtrahend 
+
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
 
 
-        // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
+        // Step 5:DONE
+        // Navigate to the UnitTests.cs file and complete the SubtractTest unit test method 
 
 
-        // Step 6: 
-        // Create a Multiply method that passes 2 integers
+        // Step 6: DONE
+        // Create a Multiply method that passes 2 integers DONE
+
+        public int Multiple(int num1, int num2)
+        {
+            return num1 * num2;
+        }
 
 
-        // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
+        // Step 7:DONE
+        // Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
 
 
-        // Step 8: 
-        // Create a Divide method that passes 2 integers
+        //Step 8: DONE
+        //Create a Divide method that passes 2 integers 
+
+        public int Divide(int num1, int num2)
+        {
+                return num1 / num2;
+        }
+
 
         // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
 
@@ -45,3 +66,18 @@ namespace UnitTestingExercise
         //*****NOW RUN ALL OF YOUR TESTS*********//
     }
 }
+
+
+//public int Divide(int num1, int num2)
+//try
+//{
+//    return num1 / num2;
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine($"{e.Message}");
+//}
+//finally
+//{
+//    //Console.WriteLine("Don't worry, this happens a lot");
+//}
